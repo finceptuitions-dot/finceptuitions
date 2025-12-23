6dfe722d8cff57c9ace67d471fe6d2c93e194d02
finceptuitions-dot <finceptuitions@gmail.com>
2025-12-23 13:41:29 -0800

    Refactor Firebase setup with new config and analytics
    Updated Firebase configuration and added analytics support.

--- firebase.js
+++ firebase.js
@@ -1,20 +1,21 @@
-// firebase.js
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC6gaEIKoeZmSRwVVJOrkQtk7SW_iiqz04",
-  authDomain: "secondary-school-center.firebaseapp.com",
-  projectId: "secondary-school-center",
-  storageBucket: "secondary-school-center.firebasestorage.app",
-  messagingSenderId: "156434969933",
-  appId: "1:156434969933:web:796b926798647d1ada3304",
-  measurementId: "G-G70CP4W5F8"
+  apiKey: "AIzaSyCBFObaNdHuPDVM3eo-1qtJ0ENQ3vf1oOc",
+  authDomain: "finceponlinetuitions.firebaseapp.com",
+  projectId: "finceponlinetuitions",
+  storageBucket: "finceponlinetuitions.firebasestorage.app",
+  messagingSenderId: "562071475466",
+  appId: "1:562071475466:web:1823cb7c22b64729dd16f3",
+  measurementId: "G-D8S53TJCZ0"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
